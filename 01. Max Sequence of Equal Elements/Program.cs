bddf1f58658f01b58f304ac90d54d2463a172eb5
WTF <genuine@dir.bg>
2016-09-20 16:29:13 +0300

    Project to manipulate with GitHub

---
+++ 01. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+        
+        List<int> result = new List<int>();
+        int winNumber = numInput[0];
+        int counter = 1;
+        int maxCounter = 1;
+
+        for (int i = 1; i < numInput.Count; i++)
+        {
+            if (numInput[i] == numInput[i - 1])
+            {
+                counter++;
+                if (counter > maxCounter)
+                {
+                    winNumber = numInput[i];
+                }
+            }
+            else
+            {
+                counter = 1;
+            }
+
+            maxCounter = Math.Max(maxCounter, counter);
+
+        }
+
+        for (int i = 0; i < maxCounter; i++)
+        {
+            Console.Write(winNumber + " ");
+        }
+    }
+}
