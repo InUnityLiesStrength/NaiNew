f292c1dede1a8793d372b794fc313fd189b36291
WTF <genuine@dir.bg>
2016-09-20 16:43:09 +0300

    desc

---
+++ desctop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+        
+        List<int> result = new List<int>();
+        int winNumber = numInput[0];
+        int counter = 1;
+        int maxCounter = 1;
+
+        for (int i = 1; i < numInput.Count; i++)
+        {
+            if (numInput[i] == numInput[i - 1])
+            {
+                counter++;
+                if (counter > maxCounter)
+                {
+                    winNumber = numInput[i];
+                }
+            }
+            else
+            {
+                counter = 1;
+            }
+
+            maxCounter = Math.Max(maxCounter, counter);
+
+        }
+//ADD TEXT for GitHub
+//New line added
+// third
+
+// desctop
+        for (int i = 0; i < maxCounter; i++)
+        {
+            Console.Write(winNumber + " ");
+        }
+    }
+}
